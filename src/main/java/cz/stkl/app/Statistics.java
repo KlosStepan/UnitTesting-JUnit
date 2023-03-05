16e254e274e5029209033fb4409796e03a97231e
Stepan Klos <stepanklos@gmail.com>
2023-03-05 23:47:27 +0100

    Testing of IFaced Employees class via Statistics progress

--- src/main/java/cz/stkl/app/Statistics.java
+++ src/main/java/cz/stkl/app/Statistics.java
@@ -1,5 +1,10 @@
 package cz.stkl.app;
 
+import java.util.Iterator;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
 public class Statistics {
     Employees ppl;
 
@@ -8,25 +13,55 @@ public class Statistics {
     }
 
     public int computeAverageSalary() {
-        // loop&calculate
-        return 35400;
+        Integer sum = 0;
+        Integer it = 0;
+        //
+        Set<Integer> ids = ppl.getAll();
+        Iterator<Integer> idsIterator = ids.iterator();
+        while (idsIterator.hasNext()) {
+            // System.out.println(idsIterator.next());
+            sum += ppl.getSalary(idsIterator.next());
+            it++;
+        }
+        return (sum / it);
     }
 
     public int getMinSalary() {
-        // loop&keepMin
-        return 14000;
+        Integer min = Integer.MAX_VALUE;
+        //
+        Set<Integer> ids = ppl.getAll();
+        Iterator<Integer> idsIterator = ids.iterator();
+        while (idsIterator.hasNext()) {
+            // System.out.println(idsIterator.next());
+            Integer currently_iterated = ppl.getSalary(idsIterator.next());
+            if (currently_iterated < min) {
+                min = currently_iterated;
+            }
+        }
+        return min;
     }
 
     void printSalariesByName() {
-        // pairs
-        // foreach (p in ppl)
-        // {
-        // take <p.name, p.salary>
-        // }
-        // pairs sort by name
-        // foreach(pair in pairs)
-        // {
-        // print(p.name, p.salary)
-        // }
+        SortedMap<String, Integer> sm = new TreeMap<String, Integer>();
+        //
+        Set<Integer> ids = ppl.getAll();
+        Iterator<Integer> idsIterator = ids.iterator();
+        // iterate all ids and prepare sorted SortedMap of .getName(id) .getSalary(id)
+        while (idsIterator.hasNext()) {
+            Integer curr_idx = idsIterator.next();
+            sm.put(ppl.getName(curr_idx), ppl.getSalary(curr_idx));
+        }
+        // sorted <name, salary> lines
+        for (String name : sm.keySet()) {
+            System.out.println("<" + name + ", " + sm.get(name) + ">");
+        }
+    }
+
+    void LoopAll() {
+        Set<Integer> ids = ppl.getAll();
+        Iterator<Integer> idsIterator = ids.iterator();
+        while (idsIterator.hasNext()) {
+            System.out.println(idsIterator.next());
+        }
     }
 }
--- src/test/java/cz/stkl/app/StatisticsTest.java
+++ src/test/java/cz/stkl/app/StatisticsTest.java
@@ -1,5 +1,6 @@
 package cz.stkl.app;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.hamcrest.Matchers;
@@ -10,5 +11,41 @@ public class StatisticsTest {
     @Test
     public void shouldAnswerWithTrue() {
         assertTrue(true);
+    }
+
+    @Test
+    public void AverageOf3Salaries() {
+        People p = new People();
+        p.add("Svoboda", 32000);
+        p.add("Hufnagl", 30000);
+        p.add("Novak", 31000);
+        Statistics s = new Statistics(p);
+        // s.LoopAll();
+        // System.out.println(s.computeAverageSalary());
+        assertEquals(s.computeAverageSalary(), 31000);
+    }
+
+    @Test
+    public void MinSalaryOf5() {
+        People p = new People();
+        p.add("Novotny", 35000);
+        p.add("Pokora", 17000);
+        p.add("Stedry", 9000);
+        p.add("Dlouhy", 25000);
+        p.add("Pospisil", 28000);
+        Statistics s = new Statistics(p);
+        assertEquals(s.getMinSalary(), 9000);
+    }
+
+    @Test
+    public void Sort3GuysPrint() {
+        People p = new People();
+        p.add("Yvorsky", 15000);
+        p.add("Brauner", 29500);
+        p.add("Zabilov", 14000);
+        p.add("Kohutek", 28000);
+        p.add("Alsovsky", 17500);
+        Statistics s = new Statistics(p);
+        s.printSalariesByName();
     }
 }
